303f9041a99a108dd3d6776e5fa3b1ad5af7aced
epdsant <epdsant@gmail.com>
2024-02-20 15:33:10 +0000

    Initial commit

---
+++ CryptoCoins/CryptoCoins/CryptoCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinsApp.swift
+//  CryptoCoins
+//
+//  Created by EDSON SANTOS on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
