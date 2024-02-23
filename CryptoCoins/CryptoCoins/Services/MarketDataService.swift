fe20033a7c08c4206d28744d6a37f396b4d60cd7
epdsant <epdsant@gmail.com>
2024-02-23 18:05:06 +0000

    feat: Downloaded and displayed live market data from API

---
+++ CryptoCoins/CryptoCoins/Services/MarketDataService.swift
@@ -0,0 +1,32 @@
+//
+//  MarketDataService.swift
+//  CryptoCoins
+//
+//  Created by EDSON SANTOS on 23/02/2024.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    var marketDataSubscription: AnyCancellable?
+    
+    init(){
+        getGlobalData()
+    }
+       
+    private func getGlobalData(){
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (retunedGlobalData) in
+                self?.marketData = retunedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+    
+}
