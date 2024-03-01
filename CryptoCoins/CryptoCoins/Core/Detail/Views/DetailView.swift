aedc4ceb0c5c72a164272ef13386cd56dc930bf1
epdsant <epdsant@gmail.com>
2024-03-01 19:32:12 +0000

    feat: Added a custom Navigation Link for lazy loading

---
+++ CryptoCoins/CryptoCoins/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,39 @@
+//
+//  DetailView.swift
+//  CryptoCoins
+//
+//  Created by EDSON SANTOS on 01/03/2024.
+//
+
+import SwiftUI
+
+struct DetailLoadingView: View {
+    @Binding var coin: CoinModel?
+    var body: some View {
+        ZStack{
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        print("Initializing Detail View for \(coin.name)")
+    }
+    
+    var body: some View {
+        Text(coin.name)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View{
+        DetailView(coin: dev.coin)
+    }
+}
